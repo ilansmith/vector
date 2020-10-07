b4789332fcbc79b16101ea02deb0f2bc8caa5063
Ilan Smith <ilansm@nvidia.com>
2020-10-08 01:56:42 +0300

    Fix vector implementation per unit tests
    Signed-off-by: Ilan Smith <lunnys@gmail.com>

--- vector.c
+++ vector.c
@@ -2,20 +2,38 @@
 #include <string.h>
 #include "vector.h"
 
-#define VEC_INIT_SIZE 4
+#define VEC_INIT_CAPACITY 4
 
 struct vector {
-	unsigned int last;
-	unsigned int size;
+	unsigned int push_back_idx;
+	unsigned int capacity;
 	void **buf;
 	vec_dtor dtor;
 };
 
-static inline unsigned int pos2size(unsigned int x)
+/* Calculate the buffer capacity required for populating a given position, such
+ * that the required capacity is double the closest power of 2 greater than
+ * pos+1:
+ *
+ * capacity = first_pow2_greater_than(pos+1) * 2 = 32
+ *
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * | | | | | | | | | | | |x| | | | | | | | | | | | | | | | | | | | |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  ^                     ^       ^                               ^
+ *  0                    pos      15                              31
+ *
+ * Algorithm:
+ *
+ * 1. Paint 1 from the MSB to the LSB
+ * 2. Add 1 for the closest power of 2 greater than the MSB
+ * 3. Double this number
+ */
+static inline unsigned int pos2capacity(unsigned int x)
 {
 	int i;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 63; i++)
 		x |= x >> (1<<i);
 	return ((x >> 1) + 1) << 2;
 }
@@ -23,26 +41,26 @@ static inline unsigned int pos2size(unsigned int x)
 static inline void buf_realloc(struct vector *vec, unsigned int pos)
 {
 	void **buf;
-	unsigned int size_required = pos2size(pos);
+	unsigned int capacity_required = pos2capacity(pos);
 	
-	if (size_required <= vec->size)
+	if (capacity_required <= vec->capacity)
 		return;
 
-	buf = realloc(vec->buf, size_required * sizeof(void*));
+	buf = realloc(vec->buf, capacity_required * sizeof(void*));
 	if (!buf)
 		return;
 
 	vec->buf = buf;
-	memset(vec->buf + vec->size, 0, size_required - vec->size);
-	vec->size = size_required;
+	memset(vec->buf + vec->capacity, 0,
+		sizeof(void*) * (capacity_required - vec->capacity));
+	vec->capacity = capacity_required;
 }
 
 static inline void do_replace(struct vector *vec, int pos, void *e)
 {
-	if (!vec->buf[pos])
-		return;
+	if (vec->buf[pos])
+		vec->dtor(vec->buf[pos]);
 
-	vec->dtor(vec->buf[pos]);
 	vec->buf[pos] = e;
 }
 
@@ -54,10 +72,10 @@ void vec_uninit(vector_t vector)
 		return;
 
 	vec = (struct vector*)vector;
-	while (vec->size) {
-		if (vec->buf[vec->size - 1])
-			vec->dtor(vec->buf[vec->size - 1]);
-		vec->size--;
+	while (vec->capacity) {
+		if (vec->buf[vec->capacity - 1])
+			vec->dtor(vec->buf[vec->capacity - 1]);
+		vec->capacity--;
 	}
 
 	free(vec->buf);
@@ -72,14 +90,14 @@ vector_t vec_init(vec_dtor dtor)
 	if (!vec)
 		return NULL;
 
-	vec->buf = calloc(VEC_INIT_SIZE, sizeof(void*));
+	vec->buf = calloc(VEC_INIT_CAPACITY, sizeof(void*));
 	if (!vec->buf) {
 		vec_uninit(vec);
 		return NULL;
 	}
 
-	vec->last = 0;
-	vec->size = VEC_INIT_SIZE;
+	vec->push_back_idx = 0;
+	vec->capacity = VEC_INIT_CAPACITY;
 	vec->dtor = dtor ? dtor : free;
 
 	return (vector_t)vec;
@@ -89,11 +107,14 @@ int vec_push_back(vector_t vector, void *e)
 {
 	struct vector *vec = (struct vector*)vector;
 
-	if (vec->last == vec->size)
+	if (!e)
 		return -1;
 
-	vec->buf[vec->last++] = e;
-	buf_realloc(vec, vec->last);
+	buf_realloc(vec, vec->push_back_idx);
+	if (vec->push_back_idx == vec->capacity)
+		return -1;
+
+	vec->buf[vec->push_back_idx++] = e;
 	return 0;
 }
 
@@ -101,14 +122,17 @@ int vec_insert(vector_t vector, void *e, unsigned int pos)
 {
 	struct vector *vec = (struct vector*)vector;
 
+	if (!e)
+		return -1;
+
 	buf_realloc(vec, pos);
-	if (vec->size <= pos)
+	if (vec->capacity <= pos)
 		return -1;
 
 	do_replace(vec, pos, e);
 
-	if (vec->last < pos)
-		vec->last = pos;
+	if (vec->push_back_idx <= pos)
+		vec->push_back_idx = pos + 1;
 
 	return 0;
 }
@@ -124,17 +148,24 @@ void *vec_at(vector_t vector, int pos)
 {
 	struct vector *vec = (struct vector*)vector;
 
-	if (vec->size <= (unsigned int)pos)
+	if (vec->capacity <= (unsigned int)pos)
 		return NULL;
 
 	return vec->buf[pos];
 }
 
-unsigned int vec_size(vector_t *vector)
+unsigned int vec_size(vector_t vector)
 {
 	struct vector *vec = (struct vector*)vector;
 
-	return vec->last;
+	return vec->push_back_idx;
+}
+
+unsigned int vec_capacity(vector_t vector)
+{
+	struct vector *vec = (struct vector*)vector;
+
+	return vec->capacity;
 }
 
 unsigned int vec_for_each(vector_t vector, int (*func)(void *e, va_list va),
@@ -145,7 +176,7 @@ unsigned int vec_for_each(vector_t vector, int (*func)(void *e, va_list va),
 	unsigned int i;
 
 	va_start(va, func);
-	for (i = 0; i < vec->size; i++) {
+	for (i = 0; i < vec->capacity; i++) {
 		if (!vec->buf[i])
 			continue;
 
@@ -161,7 +192,7 @@ void **vec_idx2addr(vector_t vector, unsigned int idx)
 {
 	struct vector *vec = (struct vector*)vector;
 
-	if (vec->last < idx)
+	if (vec->push_back_idx < idx)
 		return NULL;
 
 	return &vec->buf[idx];
--- vector.h
+++ vector.h
@@ -13,7 +13,8 @@ int vec_push_back(vector_t vector, void *e);
 int vec_insert(vector_t vector, void *e, unsigned int pos);
 void vec_erase(vector_t vector, int pos);
 void *vec_at(vector_t vector, int pos);
-unsigned int vec_size(vector_t *vector);
+unsigned int vec_size(vector_t vector);
+unsigned int vec_capacity(vector_t vector);
 unsigned int vec_for_each(vector_t vector, int (*func)(void *e, va_list va),
 	...);
 void **vec_idx2addr(vector_t vector, unsigned int idx);
